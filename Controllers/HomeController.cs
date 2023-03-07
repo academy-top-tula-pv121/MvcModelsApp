78770339687c8cd837c6a7219325be976ec85444
maxefimov <efimov.top.academy@gmail.com>
2023-03-07 21:21:14 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcModelsApp.Models;
+using System.Diagnostics;
+
+namespace MvcModelsApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<User> users = new()
+        {
+            new(1, "Bob", 27),
+            new(2, "Joe", 31),
+            new(3, "Leo", 19),
+            new(4, "Jim", 24),
+            new(5, "Sam", 36),
+        };
+
+        public HomeController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View(users);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿namespace MvcModelsApp.Models
+{
+    /*
+    Модели данных
+    Модели которые получают содержимое их хранилищ (БД, xml файлы и тп)
+
+    Модели представления
+    Модели для передачи данных в представление или получение данных из представления 
+
+    Промежуточные модели
+     */
+    public record class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public User(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+    }
+}
